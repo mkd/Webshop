f22b9528cf5be8429c19e916237ed5d05e32ca15
Claudio M. Camacho <claudio@silver.dhcp.inet.fi>
2011-02-09 23:12:51 +0200

    Revert "Updated urls.py."
    This reverts commit 70feabcbc3d20116c5916f444060ea9aea4e3b8c.
    
    Conflicts:
    
    	src/Shop/web/urls.py

--- src/Shop/web/urls.py
+++ src/Shop/web/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
+<<<<<<< HEAD
     (r'^product/(?P<product_id>)', 'web.views.product'),    
     #(r'^category/(?P<category_id>)', 'web.views.category'), 
     #(r'^user/cart/(?P<user_id>)', 'web.views.cart'),
@@ -8,5 +9,15 @@ urlpatterns = patterns('',
     (r'^index$', 'web.views.index'),
     (r'^$', 'web.views.index'),
     (r'^signup$', 'web.views.signup'),
+)
+=======
+    (r'$product/(?P<product_id>)', 'web.views.product'),    
+    (r'$category/(?P<category_id>)', 'web.views.category'), 
+    (r'$user/cart/(?P<user_id>)', 'web.views.cart'),
+    (r'$user/profile/(?P<user_id>)', 'web.views.profile'),
+    (r'index$', 'web.views.index'),
+    (r'$', 'web.views.index'),
+>>>>>>> parent of 70feabc... Updated urls.py.
+    
 )
     
