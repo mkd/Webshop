9d2a7bc093063de75e967ce7084bf077b88519f8
Claudio M. Camacho <claudio@silver.dhcp.inet.fi>
2011-02-09 23:14:54 +0200

    New front page with nice icons.

--- src/Shop/web/urls.py
+++ src/Shop/web/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-<<<<<<< HEAD
     (r'^product/(?P<product_id>)', 'web.views.product'),    
     #(r'^category/(?P<category_id>)', 'web.views.category'), 
     #(r'^user/cart/(?P<user_id>)', 'web.views.cart'),
@@ -9,15 +8,5 @@ urlpatterns = patterns('',
     (r'^index$', 'web.views.index'),
     (r'^$', 'web.views.index'),
     (r'^signup$', 'web.views.signup'),
-)
-=======
-    (r'$product/(?P<product_id>)', 'web.views.product'),    
-    (r'$category/(?P<category_id>)', 'web.views.category'), 
-    (r'$user/cart/(?P<user_id>)', 'web.views.cart'),
-    (r'$user/profile/(?P<user_id>)', 'web.views.profile'),
-    (r'index$', 'web.views.index'),
-    (r'$', 'web.views.index'),
->>>>>>> parent of 70feabc... Updated urls.py.
-    
 )
     
