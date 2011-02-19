942343a97fa037d143c753044a839ae27c2586a4
Claudio M. Camacho <claudiomkd@gmail.com>
2011-02-19 12:20:35 +0200

    Added validation functionality.
    Created static/js where JS files can be placed. Now there is a formvalidation.js
    and a validate.js to use jQuery validation functionalities. These are included
    by default in the base.html already.

---
+++ src/Shop/web/static/js/formvalidation.js
@@ -0,0 +1,49 @@
+/*
+ * formvalidation.js
+ *
+ * Here are all the rules to validate various forms in the website.
+ */
+$(document).ready(function()
+{
+  $("#submit").click(function()
+  {
+    $("#register_form").validate({
+        rules:
+        {
+            fname:
+            {
+                maxlength: 16,
+                required:  true
+            },
+            
+            sname:
+            {
+                maxlength: 16,
+                required:  true
+            },
+
+            user:
+            {
+                required:  true
+            },
+
+            pass2:
+            {
+                required:  true
+            },
+
+            email:
+            {
+                maxlength: 32,
+                required:  true
+            },
+
+            email2:
+            {
+                maxlength: 32,
+                required:  true
+            },
+        }
+    });
+  });
+});
