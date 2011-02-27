76520887d299c3012d8b91163535b78d18c86bd2
Claudio M. Camacho <claudiomkd@gmail.com>
2011-02-27 20:12:26 +0200

    Script to load valid or cool CSS with one command.

---
+++ src/Shop/web/static/css/switch_css.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+arg="$1"
+
+case $arg in
+    "std")
+        echo "Switching to valid CSS"
+        ln -sf admin_std.css admin.css
+        ln -sf cart_std.css cart.css
+        ln -sf category_std.css category.css
+        ln -sf product_std.css product.css
+        ln -sf style_std.css style.css
+    ;;
+    "cool")
+        echo "Switching to CSS3"
+        ln -sf admin_cool.css admin.css
+        ln -sf cart_cool.css cart.css
+        ln -sf category_cool.css category.css
+        ln -sf product_cool.css product.css
+        ln -sf style_cool.css style.css
+    ;;
+    *)
+        echo "Usage: $0 [std|cool]"
+        exit 1
+    ;;
+esac
